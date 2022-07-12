0492e367cf2bdb93c29cbab1878779cb43f0a0c7
Muhammad Irfan Bisyr <irfan100397@gmail.com>
2022-07-12 15:15:45 +0700

    Responsive portfolio list

--- src/lib/CompanyCard.svelte
+++ src/lib/CompanyCard.svelte
@@ -6,29 +6,50 @@
         CardHeader, 
         CardSubtitle, 
         CardText, 
+        Container,
+        Col,
+        Row
     } from 'sveltestrap';
 
     export let name = '';
     export let jobTitle = '';
     export let location = {};
     export let role = '';
+    export let start = '';
+    export let end = '';
 </script>
 
-<div>
-    <Card>
-        <CardHeader> 
-            <CardTitle> {name} </CardTitle>
-            {`${location.city}, ${location.country}`}
-        </CardHeader>
-        <CardBody>
-            <CardSubtitle>{jobTitle}</CardSubtitle>
-            <CardText>{role}</CardText>
-        </CardBody>
-    </Card>
-</div>
+<Card class='card-body-company'>
+    <CardHeader> 
+        <Container>
+            <Row>
+                <Col xs="6">
+                    <CardTitle class='card-title-name'>{name}</CardTitle>
+                    {`${location.city}, ${location.country}`}
+                </Col >
+                <Col xs="6" class='align-right'>{`${start} - ${end}`}</Col>
+            </Row>
+        </Container>
+    </CardHeader>
+    <CardBody>
+        <CardSubtitle>{jobTitle}</CardSubtitle>
+        <CardText>{role}</CardText>
+    </CardBody>
+</Card>
 
 <style>
-    .card-title {
-        margin-bottom: 0px;
+    :global(.card-body-company){
+        box-shadow: 1px 1px 5px 1px #b5b5b58f;
+        border: none !important;
+        margin-bottom: 16px;
     }
+    
+    :global(.card-title-name) {
+        margin-bottom: 0px !important;
+    }
+
+    :global(.align-right) {
+        text-align: right;
+    }
+
 </style>
--- src/lib/PortfolioList.svelte
+++ src/lib/PortfolioList.svelte
@@ -49,9 +49,17 @@
 </section>
 
 <style>
-    .portfolio-list {
-        margin: 0 auto;
-        width: 70%;
+    @media only screen and (max-width: 640px) {
+        .portfolio-list {
+            margin: 0 auto;
+            width: 100%;
+        }
+    }
+    @media only screen and (min-width: 640px) {
+        .portfolio-list {
+            margin: 0 auto;
+            width: 70%;
+        }
     }
 
     .portfolio-list h2{
