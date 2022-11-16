2538bc1ef2dfa17ddf5ff0fc30d042bb681b28e3
Muhammad Irfan Bisyr <irfan100397@gmail.com>
2022-11-16 17:37:46 +0700

    Update first page

--- src/lib/Main.svelte
+++ src/lib/Main.svelte
@@ -1,14 +1,17 @@
 <script>
     
-    import {slide} from 'svelte/transition'
-    import { Button } from 'sveltestrap'
+    import { slide } from 'svelte/transition'
+    import { Button, Container, Row, Col } from 'sveltestrap'
 
     import mainBackground from '../assets/image/main-background.jpg'
     import Logo from './global/Logo.svelte';
-    import {contacts} from './contact/Contacts'
+    import { contacts } from './contact/Contacts'
 
     import { ref, getDownloadURL } from 'firebase/storage'
     import { storage } from '../firebase'
+
+    import JapanFlag from '../assets/logo/japan.png'
+    import IndonesiaFlag from '../assets/logo/indonesia.png';
 
     $: resumeUrl = '#';        
     getDownloadURL(ref(storage, 'Resume.pdf')).then((res) => resumeUrl = res);
@@ -16,14 +19,43 @@
     export let menu;
 </script>
 
-<section class="main-section" in:slide="{{ duration: 100 }}">
+<section class="main-section">
     <div class="main-background" style="background-image: url({mainBackground});"/>
     <div class="main-text">
-        <h2>Welcome!</h2>
-        <h1>I'm Muhammad Irfan Bisyr</h1>
-        <p>
-            <span class="text-years">3</span> years experience in programmer / software engineer related field.
-        </p>
+        <h1>Welcome!</h1>
+        <h1 class="title-name">I'm Muhammad Irfan Bisyr</h1>
+        <div class="main-row">
+            <Container>
+                <Row>
+                    <Col>
+                        <div in:slide="{{ duration: 300 }}">
+                            <h1>3+ Years</h1>
+                            of Software Engineer Experience
+                        </div>
+                    </Col>
+                    <Col>
+                        <div in:slide="{{ duration: 300 }}">
+                            <h1>2 Country</h1>
+                            <img 
+                                class="country-flag"
+                                src={JapanFlag} 
+                                alt='Japan' 
+                            /> 
+                            <img 
+                                class="country-flag"
+                                src={IndonesiaFlag} 
+                                alt='Indonesia'
+                            /> 
+                        </div>
+                    </Col>
+                    <Col>
+                        <div in:slide="{{ duration: 300 }}">
+                            <h1>5+ Technology</h1>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
         <div class='button-group'>
             <Button color='primary' on:click={() => menu = 1}>View My Experience</Button>
             <Button color='info' href={resumeUrl}>Download My Resume</Button>
@@ -45,9 +77,9 @@
         margin-bottom: 8px;
     }
 
-    .text-years {
-        font-size: 48pt;
+    .title-name {
         font-weight: bold;
+        font-size: 36pt;
     }
 
     .main-section {
@@ -74,5 +106,14 @@
         padding: 10%;
         margin-left: 0.5em;
         margin-right: 0.5em;
+    }
+
+    .main-row {
+        font-weight: bold;
+        margin: 42px;
+    }
+
+    .main-row img {
+        width: 40px;
     }
 </style>
